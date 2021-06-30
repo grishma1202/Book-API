6ebfdfde6c7d6e65f53275cbf181d3043ec85a37
Grishma Sanghavi <manishasanghavi43@gmail.com>
2021-06-30 19:17:23 +0530

    add mongodb connection

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const mongoose = require("mongoose")
 const database = require("./database/index");
 
 //Models
-const BookModels = require("./database/book")
-const AuthorModels = require("./database/author")
-const PublicationModels = require("./database/publication")
+const BookModel = require("./database/book")
+const AuthorModel = require("./database/author")
+const PublicationModel = require("./database/publication")
 
 // Initializing express
 const shapeAI = express();
@@ -26,8 +26,9 @@ Access          PUBLIC
 Parameters      NONE
 Method          GET
 */
-shapeAI.get("/", (req, res) => {
-  return res.json({ books: database.books });
+shapeAI.get("/", async (req, res) => {
+  const getAllBooks = await BookModel.find();
+  return res.json({ getAllBooks });
 });
 
 //Establish database connection
@@ -48,12 +49,15 @@ Access          PUBLIC
 Parameters      isbn
 Method          GET
 */
-shapeAI.get("/is/:isbn", (req, res) => {
-  const getSpecificBook = database.books.filter(
-    (book) => book.ISBN === req.params.isbn
-  );
+shapeAI.get("/is/:isbn", async (req, res) => {
 
-  if (getSpecificBook.length === 0) {
+  const getSpecificBook = await BookModel.findOne({ISBN: req.params.isbn});
+
+  // const getSpecificBook = database.books.filter(
+  //   (book) => book.ISBN === req.params.isbn
+  // );
+
+  if (!getSpecificBook) {
     return res.json({
       error: `No book found for the ISBN of ${req.params.isbn}`,
     });
@@ -69,12 +73,15 @@ Access          PUBLIC
 Parameters      category
 Method          GET
 */
-shapeAI.get("/c/:category", (req, res) => {
-  const getSpecificBooks = database.books.filter((book) =>
-    book.category.includes(req.params.category)
-  );
+shapeAI.get("/c/:category", async (req, res) => {
 
-  if (getSpecificBooks.length === 0) {
+  const getSpecificBooks = await BookModel.findOne({category: req.params.category });
+
+  // const getSpecificBooks = database.books.filter((book) =>
+  //   book.category.includes(req.params.category)
+  // );
+
+  if (!getSpecificBooks) {
     return res.json({
       error: `No book found for the category of ${req.params.category}`,
     });
@@ -90,8 +97,9 @@ Access          PUBLIC
 Parameters      NONE
 Method          GET
 */
-shapeAI.get("/author", (req, res) => {
-  return res.json({ authors: database.authors });
+shapeAI.get("/author", async (req, res) => {
+  const getAllAuthors = await AuthorModel.find();
+  return res.json({ authors: getAllAuthors });
 });
 
 /*
@@ -135,8 +143,13 @@ Method          POST
 */
 shapeAI.post("/book/new", (req, res) => {
   const { newBook } = req.body;
-  database.books.push(newBook);
-  return res.json({ books: database.books, message: "book was added!" });
+
+  const addNewbook = BookModel.create(newBook);
+
+  
+  // const { newBook } = req.body;
+  // database.books.push(newBook);
+  return res.json({ books: addNewbook, message: "book was added!" });
 });
 
 /*
@@ -148,8 +161,10 @@ Method          POST
 */
 shapeAI.post("/author/new", (req, res) => {
   const { newAuthor } = req.body;
-  database.authors.push(newAuthor);
-  return res.json({ authors: database.authors, message: "author was added!" });
+
+  AuthorModel.create(newAuthor);
+  // database.authors.push(newAuthor);
+  return res.json({message: "author was added!" });
 });
 
 /*
